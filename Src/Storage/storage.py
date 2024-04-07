fc958f7c0c232637f460f837d9586de666a92c96
nikitastepanov <stepanov.iop@gmail.com>
2024-04-07 23:56:02 +0800

    Overwrite

--- Src/Storage/storage.py
+++ Src/Storage/storage.py
@@ -13,19 +13,18 @@ class storage():
     __data = {}
     __storage_file = "storage.json"
     __mapping = {}
-    
+
     def __new__(cls):
         if not hasattr(cls, 'instance'):
             cls.instance = super(storage, cls).__new__(cls)
 
-        return cls.instance  
-    
+        return cls.instance
+
     def __init__(self) -> None:
         # Связка для всех моделей
-        for  inheritor in reference.__subclasses__():
+        for inheritor in reference.__subclasses__():
             self.__mapping[inheritor.__name__] = inheritor
 
-    
     @property
     def data(self) -> dict:
         """
@@ -35,7 +34,7 @@ class storage():
             _type_: _description_
         """
         return self.__data
-    
+
     def load(self):
         """
             Загрузить данные из хранилища
@@ -49,14 +48,14 @@ class storage():
                 raise operation_exception(f"Невозможно загрузить данные! Не найден файл {data_file}")
 
             with open(data_file, "r") as read_file:
-                source =  json.load(read_file)   
-                
+                source = json.load(read_file)
+
                 self.__data = {}
                 for key in storage.storage_keys(storage):
                     if key in source.keys():
                         source_data = source[key]
                         self.__data[key] = []
-                        
+
                         for item in source_data:
                             object = self.__mapping[key]
                             instance = object().load(item)
@@ -64,8 +63,7 @@ class storage():
 
         except Exception as ex:
             raise operation_exception(f"Ошибка при чтении данных. Файл {self.__storage_file}\n{ex}")
-        
-        
+
     def save(self):
         """
             Сохранить данные в хранилище
@@ -75,17 +73,16 @@ class storage():
         try:
             factory = convert_factory()
             with open(self.__storage_file, "w") as write_file:
-                data = factory.serialize( self.data )
-                json_text = json.dumps(data, sort_keys = True, indent = 4, ensure_ascii = False)  
+                data = factory.serialize(self.data)
+                json_text = json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False)
                 write_file.write(json_text)
-                
+
                 return True
         except Exception as ex:
             raise operation_exception(f"Ошибка при записи файла {self.__storage_file}\n{ex}")
-            
-        return False    
 
- 
+        return False
+
     @staticmethod
     def nomenclature_key():
         """
@@ -95,7 +92,6 @@ class storage():
         """
         return "nomenclature_model"
 
-  
     @staticmethod
     def group_key():
         """
@@ -104,8 +100,7 @@ class storage():
             _type_: _description_
         """
         return "group_model"
-      
-      
+
     @staticmethod
     def storage_transaction_key():
         """
@@ -113,10 +108,9 @@ class storage():
         Returns:
             _type_: _description_
         """
-        return "storage_row_model"  
-    
+        return "storage_row_model"
 
-    @staticmethod  
+    @staticmethod
     def unit_key():
         """
               Список единиц измерения
@@ -124,7 +118,7 @@ class storage():
             _type_: _description_
         """
         return "unit_model"
-    
+
     @staticmethod
     def receipt_key():
         """
@@ -133,9 +127,9 @@ class storage():
             _type_: _description_
         """
         return "receipe_model"
-    
+
     # Код взят: https://github.com/UpTechCompany/GitExample/blob/6665bc70c4933da12f07c0a0d7a4fc638c157c40/storage/storage.py#L30
-    
+
     @staticmethod
     def storage_keys(cls):
         """
@@ -149,4 +143,3 @@ class storage():
             if method.__name__.endswith("_key") and callable(method):
                 keys.append(method())
         return keys
-    
